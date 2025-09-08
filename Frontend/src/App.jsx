bd9d87aada4cc044a81c3dfed80199d4586b8622
Harshit Halwai <harshithalwai001@gmail.com>
2025-09-08 18:11:55 +0530

    HJ

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 import Navbar from "./sections/Navbar.jsx";
 import Hero from "./sections/Hero.jsx";
-import MatterBackground from "./Components/MatterBackground.jsx";
 
 const App = () => {
   return (
     <>
       <div className="bg-slate-900 text-white font-sans overflow-x-hidden">
         <div className="container mx-auto max-w-7xl">
-          <MatterBackground />
           <Navbar />
           <Hero />
         </div>
