c8c76fb41e3493c0eec700e7bd5c76b393841c02
Harshit Halwai <harshithalwai001@gmail.com>
2025-11-12 16:57:42 +0530

    12-11-2025 fixed the social media icons

--- Frontend/src/Components/SocialIcons.jsx
+++ Frontend/src/Components/SocialIcons.jsx
@@ -1,26 +1,65 @@
 import React from "react";
 import {
-  TelegramIcon,
-  YouTubeIcon,
-  WhatsAppIcon,
-  InstagramIcon,
-  MailIcon,
-  GitHubIcon,
-  GeeksforGeeksIcon,
-  LeetCodeIcon,
-  LinkedInIcon, 
-} from "./MenuIcons.jsx";
+  FaTelegramPlane,
+  FaYoutube,
+  FaWhatsapp,
+  FaInstagram,
+  FaEnvelope,
+  FaGithub,
+  FaLinkedin,
+} from "react-icons/fa";
+import { SiGeeksforgeeks, SiLeetcode } from "react-icons/si";
 
 const SocialIcons = () => {
   const icons = [
-    { Icon: YouTubeIcon, link: "https://www.youtube.com/@Hacked_Coder" },
-    { Icon: WhatsAppIcon, link: "https://wa.me/6388163046" },
-    { Icon: InstagramIcon, link: "https://instagram.com/harshitgupta1327" },
-    { Icon: MailIcon, link: "mailto:harshithalwai001@gmail.com" },
-    { Icon: LinkedInIcon, link: "https://in.linkedin.com/in/harshit-gupta-b80329297" },
-    { Icon: GitHubIcon, link: "https://github.com/harshithalwai" },
-    { Icon: GeeksforGeeksIcon, link: "https://www.geeksforgeeks.org/user/harshithalwai001/" },
-    { Icon: LeetCodeIcon, link: "https://leetcode.com/u/harshithalwai001/" },
+    {
+      Icon: FaYoutube,
+      link: "https://www.youtube.com/@Hacked_Coder",
+      label: "YouTube",
+      color: "hover:text-red-500",
+    },
+    {
+      Icon: FaWhatsapp,
+      link: "https://wa.me/6388163046",
+      label: "WhatsApp",
+      color: "hover:text-green-400",
+    },
+    {
+      Icon: FaInstagram,
+      link: "https://instagram.com/harshitgupta1327",
+      label: "Instagram",
+      color: "hover:text-pink-500",
+    },
+    {
+      Icon: FaEnvelope,
+      link: "mailto:harshithalwai001@gmail.com",
+      label: "Email",
+      color: "hover:text-yellow-400",
+    },
+    {
+      Icon: FaLinkedin,
+      link: "https://in.linkedin.com/in/harshit-gupta-b80329297",
+      label: "LinkedIn",
+      color: "hover:text-blue-500",
+    },
+    {
+      Icon: FaGithub,
+      link: "https://github.com/harshithalwai",
+      label: "GitHub",
+      color: "hover:text-gray-300",
+    },
+    {
+      Icon: SiGeeksforgeeks,
+      link: "https://www.geeksforgeeks.org/user/harshithalwai001/",
+      label: "GeeksforGeeks",
+      color: "hover:text-green-500",
+    },
+    {
+      Icon: SiLeetcode,
+      link: "https://leetcode.com/u/harshithalwai001/",
+      label: "LeetCode",
+      color: "hover:text-orange-400",
+    },
   ];
 
   return (
@@ -29,21 +68,21 @@ const SocialIcons = () => {
         fixed right-3 top-1/2 -translate-y-1/2
         flex flex-col items-center
         space-y-4 md:space-y-5
-        z-50
-        bg-transparent
+        z-50 bg-transparent
       "
     >
-      {icons.map(({ Icon, link }, index) => (
+      {icons.map(({ Icon, link, label, color }, index) => (
         <a
           key={index}
           href={link}
           target="_blank"
           rel="noopener noreferrer"
-          className="
-            text-white hover:text-blue-400
-            transition-colors duration-300
+          aria-label={label}
+          className={`
+            text-white transition-all duration-300
             transform hover:scale-110
-          "
+            ${color}
+          `}
         >
           <Icon className="w-6 h-6 md:w-7 md:h-7" />
         </a>
